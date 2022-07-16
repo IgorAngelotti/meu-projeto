c8dd5a1a626bf0f88c8208611fd4d9c2e15e4354
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2022-07-16 14:45:20 -0300

    Soma

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
-var a = window.document.getElementById('res')
-function somar() {
-    a.innerText = 'VAI SE FUDER'
+var res = window.document.getElementById('res')
+var botao = document.getElementById('botaosoma')
+var num1 = document.getElementById('txtnum1')
+var num2 = document.getElementById('txtnum2')
+botao.addEventListener('click', soma)
+function soma (){
+   var somar = Number(num1.value) + Number(num2.value)
+   res.innerHTML = `Resultado: ${somar}` 
 }
