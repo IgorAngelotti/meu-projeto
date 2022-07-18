28b44007b7f61157952fff22128ced0c281ababe
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2022-07-18 19:38:30 -0300

    funcao
    teste de verificação com uma chamada para function

--- javascript.js
+++ javascript.js
@@ -3,7 +3,22 @@ var botao = document.getElementById('botaosoma')
 var num1 = document.getElementById('txtnum1')
 var num2 = document.getElementById('txtnum2')
 botao.addEventListener('click', soma)
+
+function verificação(n1, n2){ 
+   if(n1.value.length == 0 || n2.value.length == 0){
+      return false
+   } else{
+      return true
+   }
+}
+
 function soma (){
-   var somar = Number(num1.value) + Number(num2.value)
-   res.innerHTML = `Resultado: ${somar}` 
+   if(verificação(num1, num2)){ //VALIDAÇÃO COM CHAMADA PARA FUNCTION
+      var somar = Number(num1.value) + Number(num2.value)
+      res.innerHTML = `Resultado: ${somar}` 
+   } else{
+      res.innerHTML = 'Por favor, digite dois números nos campos acima.'
+   }
+   
+   
 }
